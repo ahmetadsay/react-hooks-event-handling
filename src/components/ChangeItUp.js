5e30fe28d562bb54edcf0391a64ff14c54cb7a57
ahmet <adsayahmet94@hotmail.com>
2023-06-10 04:20:16 +0300

    done

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -1,5 +1,3 @@
-import React from "react";
-
 function ChangeItUp() {
   function handleChange(event) {
     console.log(`${event.target.name}: ${event.target.value}`);
@@ -23,4 +21,4 @@ function ChangeItUp() {
   );
 }
 
-export default ChangeItUp;
+export default ChangeItUp
